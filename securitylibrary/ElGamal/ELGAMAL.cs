18a5e644d5428f43dd5bfcb11233f59291db0a08
marvelsamuel <76208265+marvelsamuel@users.noreply.github.com>
2023-04-27 14:01:05 +0200

    Update ELGAMAL.cs

--- securitylibrary/ElGamal/ELGAMAL.cs
+++ securitylibrary/ElGamal/ELGAMAL.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,13 +18,43 @@ namespace SecurityLibrary.ElGamal
         /// <returns>list[0] = C1, List[1] = C2</returns>
         public List<long> Encrypt(int q, int alpha, int y, int k, int m)
         {
-            throw new NotImplementedException();
+            // Calculate C1 and C2 
+            long C1 = Modwithpower(alpha, k, q);
+            long C2 = (m * Modwithpower(y, k, q)) % q;
 
+            return new List<long> { C1, C2 };
         }
+
         public int Decrypt(int c1, int c2, int x, int q)
         {
-            throw new NotImplementedException();
+            // Calculate the decryption exponent d 
+            long d = Modwithpower(c1, q - 1 - x, q);
 
+            // plaintext by dividing C2 by d modulus q
+            int m = (int)((c2 * d) % q);
+
+            return m;
+        }
+
+        private long Modwithpower(long baseV, long exp, long modulus)
+        {
+           
+            if (modulus == 1)
+                return 0;
+
+            long c = 1;
+            baseV %=  modulus;
+            while (exp > 0)
+            {
+                if (exp % 2 == 1)
+                {
+                    c = (c * baseV) % modulus;
+                }
+                exp /= 2;
+                baseV = (baseV * baseV) % modulus;
+            }
+
+            return c;
         }
     }
 }
